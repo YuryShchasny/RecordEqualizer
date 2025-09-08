ffafabb4899561efdef77cd756b0c51deb032477
YuryShchasny <yuryshchasnydev@gmail.com>
2025-09-08 13:04:34 +0300

    fix: scrollable equalizer screen

--- features/equalizer/src/main/java/com/sb/equalizer/presentation/ui/EqualizerContent.kt
+++ features/equalizer/src/main/java/com/sb/equalizer/presentation/ui/EqualizerContent.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -66,7 +68,9 @@ private fun EqualizerScreenContent(
             onDismiss = { showSaveDialog = false }
         )
         Column(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             Row(
